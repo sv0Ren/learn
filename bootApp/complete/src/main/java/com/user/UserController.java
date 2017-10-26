338ce7edd13e5c18ec03b0a96cdd8ce71ce39528
sven.wagner3@gmail.com <wagner@phillips.local>
2017-10-26 19:58:17 +0200

    lalal

--- bootApp/complete/src/main/java/com/user/UserController.java
+++ bootApp/complete/src/main/java/com/user/UserController.java
@@ -4,9 +4,11 @@ package com.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/user")
 public class UserController{
 
 
@@ -18,19 +20,18 @@ public class UserController{
 
     private User testUser = new User("deine","mudda");
 
-
-
-    @PostMapping("/user")
-    //@RequestMapping(method = RequestMethod.POST, value = "/")
+    @PostMapping()
     public User create(@RequestBody User user) {
         return userRepo.save(testUser);
     }
 
-    /*
+    @GetMapping(value="/{userId}")
+    public User get(@PathVariable String userId){
+        return userRepo.findOne(userId);
+    }
 
-
-    @RequestMapping(method = RequestMethod.GET, value = "/{user}")
-    public User get(@PathVariable String user) {
-        return userRepo.findOne(user);
-    }*/
+    @GetMapping(value="")
+    public List<User> getAll(){
+        return userRepo.findAll();
+    }
 }
