1928f6d198eba8612da698b72d4e591c9f4c1b1c
wagner <sven.wagner3@gmail.com>
2017-10-29 17:01:32 +0100

    ka

---
+++ webBackend/src/main/java/com/webbackend/WebbackendApplication.java
@@ -0,0 +1,12 @@
+package com.webbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbackendApplication.class, args);
+	}
+}
